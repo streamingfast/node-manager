a2cbf7e458580a1bea7fb4f3b03ff86b0d0a75ab
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2021-03-31 12:06:40 -0400

    ensure operator catching process Stop() does not cause error if supervisor is already shutting down (for batch end-block)

--- operator/operator.go
+++ operator/operator.go
@@ -162,8 +162,8 @@ func (o *Operator) Launch(startOnLaunch bool, httpListenAddr string, options ...
 	for {
 		o.zlogger.Info("operator ready to receive commands")
 		select {
-		case <-o.Superviser.Stopped(): // stopped outside of a command that was expecting it.
-			if o.IsTerminating() { // This is the natural way of exiting this loop on global shutdown.
+		case <-o.Superviser.Stopped(): // the chain stopped outside of a command that was expecting it.
+			if o.Superviser.IsTerminating() || o.IsTerminating() { // This is the natural way of exiting this loop on global shutdown.
 				return nil
 			}
 			if o.attemptedAutoRestore || time.Since(o.lastStartCommand) > 10*time.Second {
